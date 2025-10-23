ab6e914f0d319353ebc5e69287b64628043157bb
Devansh Nair <devanshnair.05@gmail.com>
2025-10-24 00:55:04 +0530

    Add user profile management features: implement password change, account update, avatar and cover image upload, and fetch current user details; enhance user routes with new endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -76,7 +76,7 @@ const registerUser = asyncHandler( async (req, res) => {
     const coverImage = await uploadOnCloudinary(coverImageLocalFilePath)
 
     if(!avatar){
-        throw new ApiError(400, "Avatar File is required!")
+        throw new ApiError(400, "Error while uploading avatar!")
     }
 
     // 4. Create user object - create entry in db
@@ -199,7 +199,7 @@ const refreshAccessToken = asyncHandler( async (req, res) => {
     }
 
     try {
-        const decodedToken = jwt.verify(incomingRefreshToken, process.env.ACCESS_TOKEN_SECRET)
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
     
         const user = await User.findById(decodedToken._id)
     
@@ -232,4 +232,345 @@ const refreshAccessToken = asyncHandler( async (req, res) => {
     }
 })
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken }
+const changeCurrentPassword = asyncHandler( async (req, res) => {
+    const { oldPassword, newPassword } = req.body
+
+    const user = await User.findById(req.user?._id)
+
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(40, "Incorrect old password!")
+    }
+
+    user.password = newPassword;
+    const userSaved = await user.save({validateBeforeSave: false});
+    console.log(userSaved)
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully!"))
+})
+
+const getCurrentUser = asyncHandler( async (req, res) => {
+    return res
+    .status(200)
+    .json(new ApiResponse(200, req.user, "Current user fetched successfully!"))
+})
+
+const updateAccountDetails = asyncHandler( async (req, res) => {
+    const { fullName, email } = req.body
+
+    if(!fullName || !email){
+        throw new ApiError(400, "All fields required")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                fullName,
+                email,
+            }
+        },
+        {
+            new: true
+        }
+    ).select("-password -refreshToken")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Account details updated successfully!"));
+})
+
+
+//TODO create 'deleteFromCloudinary' utility function and delete previous avatar and cover image from cloudinary during updation
+const updateUserAvatar = asyncHandler( async (req, res) => {
+    const avatarLocalFilePath = req.file?.path
+
+    if(!avatarLocalFilePath){
+        throw new ApiError(400, "Avatar File Missing!")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalFilePath)
+
+    if(!avatar.url){
+        throw new ApiError(400, "Error while uploading avatar")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                avatar: avatar.url,
+            }
+        },
+        { 
+            new: true,
+        }
+    ).select("-password -refreshToken")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Avatar updated successfully!"))
+})
+
+const updateUserCoverImage = asyncHandler( async (req, res) => {
+    const coverImageLocalFilePath = req.file?.path
+
+    if(!coverImageLocalFilePath){
+        throw new ApiError(400, "coverImage File Missing!")
+    }
+
+    const coverImage = await uploadOnCloudinary(coverImageLocalFilePath)
+
+    if(!coverImage.url){
+        throw new ApiError(400, "Error while uploading coverImage")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set: {
+                coverImage: coverImage.url,
+            }
+        },
+        { 
+            new: true,
+        }
+    ).select("-password -refreshToken")
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user, "Cover Image updated successfully!"))
+})
+
+const getUserChannelProfile = asyncHandler( async (req, res) => {
+    const { username } = req.params
+
+    if(!username){
+        throw new ApiError(400, "Username is missing")
+    }
+
+    const channel = await User.aggregate([
+        // Out of all User documents, you filter out single user document whose username matches.
+        //& Sample Output:   
+        //& {
+        //&     _id: ObjectId("johndoe_id"),
+        //&     username: "johndoe",
+        //&     fullName: "John Doe",
+        //&     email: "john@example.com",
+        //&     ..... other fields
+        //& }
+        {
+            $match: {
+                username: username?.toLowerCase(), 
+            }
+        },
+
+        //^ (Please note that subscriber & channel both store ObjectId which are references to User schema.
+        //^ However, they do not store the entire schema of user (username, email etc.) just a single ObjectId 
+        //^ similar to _id. It is said to be referenced because this ObjectId matches an exist user document _id.
+        //^ For e.g. a subscription document may look like 
+        //^ { _id: ObjectId("abc123"), subscriber: ObjectId("user1_id"), channel: ObjectId("user2_id"), createdAt: ISODate("...") }
+        //^ similar case in channel.)
+
+        // In subscriptions, you filter out all documents having channel matching with _id. Each stage returns
+        // an array of results. Here we get an array field called 'subscribers : []' attached to other 
+        // user details. 
+        //& Sample Output:   
+        //& {
+        //&     _id: ObjectId("johndoe_id"),
+        //&     username: "johndoe",
+        //&     fullName: "John Doe",
+        //&     email: "john@example.com",
+        //&     ..... other fields
+        //&     subscribers: [  // ← NEW FIELD ADDED (array of subscription documents)
+        //&         {
+        //&             _id: ObjectId("sub1"),
+        //&             subscriber: ObjectId("alice_id"),
+        //&             channel: ObjectId("johndoe_id"),
+        //&             createdAt: "..."
+        //&         },
+        //&         {},
+        //&         {},
+        //&     ]
+        //& }
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers",
+            }
+        },
+
+        // In subscriptions, you filter out all documents having subscriber matching with _id. 
+        // We get an array field called 'subscribedTo : []' attached to other user details and subscribed: [] 
+        //& Sample Output:   
+        //& {
+        //&     _id: ObjectId("johndoe_id"),
+        //&     username: "johndoe",
+        //&     fullName: "John Doe",
+        //&     email: "john@example.com",
+        //&     ..... other fields
+        //&     subscribers: [ {}, {}, {} ],
+        //&     subscribedTo: [  // ← NEW FIELD ADDED (channels johndoe subscribed to)
+        //&         {
+        //&             _id: ObjectId("sub10"),
+        //&             subscriber: ObjectId("johndoe_id"),
+        //&             channel: ObjectId("tech_channel_id"),
+        //&             createdAt: "..."
+        //&         },
+        //&         {},
+        //&         {},
+        //&     ]
+        //& }
+        {
+            $lookup: {
+                from: "subscriptions",
+                localField: "_id",
+                foreignField: "subscriber",
+                as: "subscribedTo",
+            }
+        },
+
+        // Here, the array subscribers and subscribedTo are replaced with the count, 
+        // because we use the same name. If we were to use different names like 
+        // 'subscriberCount' and 'channelsSucbscribedTo' then we would have previous arrays as well as new fields
+        {
+            $addFields: {
+                subscribers: {
+                    $size: "$subscribers" 
+                },
+                subscribedTo: {
+                    $size: "$subscribedTo" 
+                },
+                isSubscribed: {
+                    $cond: {
+                        // $in finds whether value exists in array/objects (both possible)
+                        // What this does is, it creates an array of only subscriber ids and searches in that
+                        //     From --->
+                        //     [  // From stage 2 (3 subscription docs)
+                        //         { _id: ObjectId("sub1"), subscriber: ObjectId("alice_id"), channel: ObjectId("johndoe_id") },
+                        //         { _id: ObjectId("sub2"), subscriber: ObjectId("bob_id"), channel: ObjectId("johndoe_id") },
+                        //         { _id: ObjectId("sub3"), subscriber: ObjectId("charlie_id"), channel: ObjectId("johndoe_id") }
+                        //     ],
+                        //     To --->
+                        //&    [ObjectId("alice_id"), ObjectId("bob_id"), ...]
+                        if: { $in: [req.user?._id, "$subscribers.subscriber"] }, 
+                        then: true,
+                        else: false,
+                    }
+                }
+            }
+        },
+        // $project simply used to specify what fields are to be shown finally in final result, 
+        // basically to remove unnecessary fields
+        {
+            $project: {
+                username: 1,
+                fullName: 1,
+                email: 1,
+                subscribers: 1,
+                subscribedTo: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+            }
+        }
+    ])
+    console.log("Channel: ", channel);
+
+    if(!channel?.length){
+        throw new ApiError(404, "Channel does not exist")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, channel[0], "Channel details fetched succesfully!"))
+}) 
+
+const getWatchHistory = asyncHandler( async (req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                // Very Important! 
+                // req.user._id comes from my own auth middleware, which fetches from mongodb and inserts
+                // So, its type is ObjectId. However, if id were to received from let's say params/queries
+                // we will get id of type String. Now, in mongoDB calls, like findbyId() etc. it accepts both String and ObjectId
+                // e.g. findById("52773") or findById("ObjectId("52773")) both will work.
+                // Note that I am talking about type 'ObjectId', just writing 'ObjectId("id")' does not change its type it is still string.
+                // However, in mongo aggregation pipelines, it does not automatically convert strings to type ObjectId
+                // Therefore, in case, id where to be fetched from params/queries, we would have to write:-
+                //& const { id } = req.params
+                //& $match: { 
+                //&     _id: new mongoose.Types.ObjectId(id) // manual conversion needed
+                //& }
+
+                _id: req.user?._id
+            }
+        },
+        // we are looking for all videos which match watchHistory. Now watchHistory is an array so
+        // lookup iterates through all id's in watchHistory and gets all matching videos.
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        // In all video documents fetched, there is an 'owner' field which is ObjectId of User model
+                        // To get actual details of owner we run a sub-pipeline. Owner (Objectid) of each video is 
+                        // matched to User documents to get details. Now we run another sub-pipeline to project only
+                        // required information i.e. username, fullName (not really necessary) and avatar.
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1,
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    // 'owner' returned will also be an array with a single object as it is a pipeline result only.
+                    // so owner: [{}]. To make life of frontend devs easier, we replace it with 1st element of array
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner" // replaces 'owner' with 'first' element of '$owner' array
+                                //& $arrayElemAt: ["$owner", 0] // replaces 'owner' with '0' index element in array '$owner'
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, user[0].watchHistory, "User's watch history fetched successfully"))
+})
+
+export { 
+    registerUser, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+}
